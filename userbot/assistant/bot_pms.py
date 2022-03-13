bd814d7d18f276b7911245b26a41fe56328178b3
Mazdak Pakaghideh <Mazdak.software@gmail.com>
2022-03-13 20:16:43 +0330

    Changed bot message

--- userbot/assistant/bot_pms.py
+++ userbot/assistant/bot_pms.py
@@ -105,17 +105,8 @@ async def bot_start(event):
         else:
             start_msg = f"Hey! 👤{mention},\
                         \nI am {my_mention}'s assistant bot.\
-                        \nYou can contact to my master from here.\
-                        \n\nPowered by [Catuserbot](https://t.me/catuserbot)"
-        buttons = [
-            (
-                Button.url("Repo", "https://github.com/TgCatUB/catuserbot"),
-                Button.url(
-                    "Deploy",
-                    "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FMr-confused%2Fcatpack&template=https%3A%2F%2Fgithub.com%2FMr-confused%2Fcatpack",
-                ),
-            )
-        ]
+                        \nYou can contact to my boss from here."
+        buttons = None
     else:
         start_msg = "Hey Master!\
             \nHow can i help you ?"
